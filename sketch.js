2b3eb3ad6d87ceff448c9ac14bf01deef91e32c8
Sandy Esses <sandy.esses@mdycehs.net>
2017-03-23 18:47:47 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(800, 800);
+}
+
+
+function draw() {
+    
+    stroke(7, 75, 186);
+    strokeWeight(10);
+    fill(255, 255, 255, 1)
+    ellipse(150, 150, 160,160);
+    
+    stroke(0, 0, 2);
+    strokeWeight(10);
+    fill(255, 255, 255, 1)
+    ellipse(325, 150, 160,160);
+    
+    stroke(226, 14, 6);
+    strokeWeight(10);
+    fill(255, 255, 255, 1)
+    ellipse(500, 150, 160,160);
+    
+    stroke(114, 244, 26);
+    strokeWeight(10);
+    fill(255, 255, 255, 1)
+    ellipse(420, 240, 160,160);
+    
+    stroke(245, 249, 14);
+    strokeWeight(10);
+    fill(255, 255, 255, 1)
+    ellipse(245, 240, 160,160);
+    
+}
